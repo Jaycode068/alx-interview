da259e8caa5cb42df3f60e019ca9b17bf8fd23b5
Jaycode068 <ajayijohnson68@yahoo.com>
2023-08-31 16:24:34 +0000

    N-Queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,77 +1,91 @@
 #!/usr/bin/python3
-""" N Queens Interview Question """
+'''N Queens Challenge'''
+
 import sys
 
 
-try:
-    size = int(sys.argv[1])
-except IndexError:
-    print("Usage: nqueens N")
-    sys.exit(1)
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
-else:
-    if size and size < 4:
-        print("N must be at least 4")
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
         sys.exit(1)
 
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        exit(1)
 
-solutions = []
-positions = None
+    if n < 4:
+        print('N must be at least 4')
+        exit(1)
 
+    solutions = []
+    placed_queens = []  # coordinates format [row, column]
+    stop = False
+    r = 0
+    c = 0
 
-def can_attacc(p1, p2):
-    """ Confirms if two queens are attacking each other """
-    if (p1[0] == p2[0]) or (p1[1] == p2[1]):
-        return True
-    return abs(p1[0] - p2[0]) == abs(p1[1] - p2[1])
+    # iterate thru rows
+    while r < n:
+        goback = False
+        # iterate thru columns
+        while c < n:
+            # check is current column is safe
+            safe = True
+            for cord in placed_queens:
+                col = cord[1]
+                if(col == c or col + (r-cord[0]) == c or
+                        col - (r-cord[0]) == c):
+                    safe = False
+                    break
 
+            if not safe:
+                if c == n - 1:
+                    goback = True
+                    break
+                c += 1
+                continue
 
-def group_check(group):
-    """ Check if a group is in solutions """
-    global solutions
-    for sol in solutions:
-        i = 0
-        for pos in sol:
-            for grp_pos in group:
-                if pos[0] == grp_pos[0] and pos[1] == grp_pos[1]:
-                    i += 1
-        if i == size:
-            return True
-    return False
+            # place queen
+            cords = [r, c]
+            placed_queens.append(cords)
+            # if last row, append solution and reset all to last unfinished row
+            # and last safe column in that row
+            if r == n - 1:
+                solutions.append(placed_queens[:])
+                for cord in placed_queens:
+                    if cord[1] < n - 1:
+                        r = cord[0]
+                        c = cord[1]
+                for i in range(n - r):
+                    placed_queens.pop()
+                if r == n - 1 and c == n - 1:
+                    placed_queens = []
+                    stop = True
+                r -= 1
+                c += 1
+            else:
+                c = 0
+            break
+        if stop:
+            break
+        # on fail: go back to previous row
+        # and continue from last safe column + 1
+        if goback:
+            r -= 1
+            while r >= 0:
+                c = placed_queens[r][1] + 1
+                del placed_queens[r]  # delete previous queen coordinates
+                if c < n:
+                    break
+                r -= 1
+            if r < 0:
+                break
+            continue
+        r += 1
 
-
-def build(row, group):
-    """ Builds a solution """
-    global solutions
-    global size
-    if row == size:
-        tmp0 = group.copy()
-        if not group_check(tmp0):
-            solutions.append(tmp0)
-    else:
-        for col in range(size):
-            a = (row * size) + col
-            matches = zip(list([positions[a]]) * len(group), group)
-            used_positions = map(lambda x: can_attacc(x[0], x[1]), matches)
-            group.append(positions[a].copy())
-            if not any(used_positions):
-                build(row + 1, group)
-            group.pop(len(group) - 1)
-
-
-def get_solutions():
-    """
-        Gets the solutions for the specified chessboard size.
-    """
-    global positions, size
-    positions = list(map(lambda x: [x // size, x % size], range(size ** 2)))
-    a = 0
-    group = []
-    build(a, group)
-
-
-get_solutions()
-for sol in solutions:
-    print(sol)
+    for idx, val in enumerate(solutions):
+        if idx == len(solutions) - 1:
+            print(val, end='')
+        else:
+            print(val)
